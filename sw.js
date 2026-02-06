fef8f3a35a045b946add60d8755e3aa8167ea7f3
be3uchii <standofd2axselboltnice19@gmail.com>
2026-02-06 20:21:33 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+var CACHE_NAME = 'messenger-v1';
+var urlsToCache = [
+  '/messenger/',
+  '/messenger/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method !== 'GET') return;
+  if (event.request.url.includes('supabase')) return;
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      if (response && response.status === 200) {
+        var responseClone = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseClone);
+        });
+      }
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
